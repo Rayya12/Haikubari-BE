ef12fab5087fa87c42e2778107d1318c58050d43
Rayya12 <rayaalului@gmail.com>
2026-01-24 14:42:09 +0700

    feat: completing authSchema.py

--- app/schema/authSchema.py
+++ app/schema/authSchema.py
@@ -1,12 +1,22 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
 from fastapi_users import schemas
 import uuid
+from typing import Literal
 
 class UserRead(schemas.BaseUser[uuid.UUID]):
-    pass
-
+    username:str
+    role:str
+    age:int | None
+    photo_url:str |None
+    file_name:str |None
+    file_type:str |None
+    bio:str |None
+    address:str |None
+    
 class UserCreate(schemas.BaseUserCreate):
-    pass
-
+    username:str
+    role: Literal["common","watcher"]
+    age: int = Field(default=1,ge=1)
+    
 class UserUpdate(schemas.BaseUserUpdate):
     pass
